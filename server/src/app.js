c174c3a19ebfe0d7e57d41264aeff90fcaac005d
ashish-138 <134204476+ashish-138@users.noreply.github.com>
2025-09-05 17:59:50 +0530

    Update app.js

--- server/src/app.js
+++ server/src/app.js
@@ -2,6 +2,7 @@ import express from 'express'
 import cors from 'cors'
 import helmet from 'helmet'
 import morgan from 'morgan'
+import cookieParser from 'cookie-parser';
 import errorHandler from '../src/middlewares/error.middleware.js'
 
 
@@ -12,6 +13,7 @@ app.use(cors())
 app.use(express.json({limit:"16kb"}))
 app.use(express.urlencoded({extended:true, limit:"16kb"}))
 app.use(express.static("public"))
+app.use(cookieParser());
 app.use(morgan('combined'))
 
 
