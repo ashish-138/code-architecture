ec0af665da9db41c130c682fe05a031dcc03adbf
ashish-138 <134204476+ashish-138@users.noreply.github.com>
2025-09-09 15:04:22 +0530

    Update app.js

--- server/src/app.js
+++ server/src/app.js
@@ -9,7 +9,12 @@ import errorHandler from '../src/middlewares/error.middleware.js'
 const app = express()
 
 app.use(helmet())
-app.use(cors())
+app.use(cors({
+  origin: (origin, callback) => {
+    callback(null, origin); 
+  },
+  credentials: true
+}));
 app.use(express.json({limit:"16kb"}))
 app.use(express.urlencoded({extended:true, limit:"16kb"}))
 app.use(express.static("public"))
